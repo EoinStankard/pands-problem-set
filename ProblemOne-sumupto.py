4171045056f37566126e17ef254e4914fff4c0eb
Eoin Stankard <g00300390@gmit.ie>
2019-02-25 20:42:04 +0000

    First draft of Problem One

---
+++ ProblemOne-sumupto.py
@@ -0,0 +1,4 @@
+#Name: Eoin Stankard
+#Date: 25/02/2019
+#Problem One:Write a program that asks the user to input any positive integer and outputs the
+#sum of all numbers between one and that number.

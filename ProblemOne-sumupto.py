94d288f36e36d248e1ff5dc7a72907c62e6243bc
Eoin Stankard <g00300390@gmit.ie>
2019-02-25 21:02:25 +0000

    Commit of basic functionality Problem One

--- ProblemOne-sumupto.py
+++ ProblemOne-sumupto.py
@@ -2,3 +2,13 @@
 #Date: 25/02/2019
 #Problem One:Write a program that asks the user to input any positive integer and outputs the
 #sum of all numbers between one and that number.
+
+i=int(input("Enter a positive integer: "))
+
+ans ,x= 0,0
+
+while x != i:
+    x = x + 1
+    ans = ans+x
+ 
+print(ans)

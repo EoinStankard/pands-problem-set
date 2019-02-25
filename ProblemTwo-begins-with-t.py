1e90acd8779fb617a7459b434dd22edc7253b148
Eoin Stankard <g00300390@gmit.ie>
2019-02-25 21:13:42 +0000

    Commit of basic functionality for Problem Two with comments

--- ProblemTwo-begins-with-t.py
+++ ProblemTwo-begins-with-t.py
@@ -4,8 +4,8 @@
 #letter T. An example of running this program on a Thursday is as follows.
 
 import datetime
-day = datetime.datetime.today().weekday()
-if (day == 1) or (day == 3):
+day = datetime.datetime.today().weekday() #Gets the day of the week
+if (day == 1) or (day == 3): # 1=Tuesday and 3= Thursday
   print("Yes. Today begins with T")
 else:
   print("No. Today does not begin with T", day)

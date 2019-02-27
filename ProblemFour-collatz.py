eb6bd514babb21a3c7533a11e0d9a597985ce99f
Eoin Stankard <g00300390@gmit.ie>
2019-02-27 22:27:03 +0000

    Committing basic functionality with all values stored in a list

--- ProblemFour-collatz.py
+++ ProblemFour-collatz.py
@@ -6,11 +6,16 @@
 #it by three and add one. Have the program end if the current value is one
 
 value=int(input("Please enter a positive integer: "))
-
+x = 0
+l=[]
 while value != 1: #While the value is not equal to 1
     if value%2 ==0: # if the current value divides evenly Even
         value = value/2 # Divide the current value by two
-        print(int(value))
+        l.insert(x,int(value))
+        x=x+1
     else: #Else if the current value does not divide evenly 
         value = (value*3)+1 #Multiply the current value by three and add one
-        print(int(value))
+        l.insert(x,int(value))
+        x=x+1
+
+print(l)

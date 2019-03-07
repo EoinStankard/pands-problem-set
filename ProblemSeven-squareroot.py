d6d4bdfa620499d5a44cdf56d7ddbd7013a2ec2b
Eoin Stankard <g00300390@gmit.ie>
2019-03-07 21:06:37 +0000

    Committing changes That ask for decimal places

--- ProblemSeven-squareroot.py
+++ ProblemSeven-squareroot.py
@@ -5,11 +5,11 @@
 import math
 #Asks for an psoitive input value
 value = float(input("Please enter a positive number: "))
-
+dec = int(input("Number of decimal places: "))
 #Created a variable called root that would hold the value of the square root
 #Imported the math library and then used the sqrt function to calculate the square root
 root = math.sqrt(value)
 #Used the round function to round the square root to one decimal place
-ans = round(root,1)
+ans = round(root,dec)
 print(ans)
 

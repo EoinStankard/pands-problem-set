e85876da0ab250b4e2b0818d6bfbb5df759713eb
Eoin Stankard <g00300390@gmit.ie>
2019-03-13 20:39:11 +0000

    ProblemSix Finished

--- ProblemSix-secondstring.py
+++ ProblemSix-secondstring.py
@@ -1,34 +1,21 @@
 #Name: Eoin Stankard
-#Date: 28/02/2019
+#Date: 13/03/2019
 #Problem Six: Write a program that takes a user input string and outputs every second word
 
-string = str(input("Please enter a sentence: "))
-#split the input string
-spl = string.split(" ")
-#Add each word to a list
-#I done this as i thought it would be easiest to print every
-#second word
-l = list(spl)
-#Second list created to store every second word
-#This would make it easy to print all words together
-l2 = []
-#print(len(l))
+string = input("Please enter a sentence: ")
 
-#variable to count every second word
-x=0
+spl = string.split(" ")#split the input string using spaces
+
+l = list(spl)#Add each word to a list
+l2 = []#Second list created to store every second word
+x = 0#variable to count every second word
+
 #while loop to go through the list
-#Even length string will finish on the last word
-#Odd number length string will finish on the second last word
+#Even length string will finish on the last word (Even amount of words)
+#Odd number length string will finish on the second last word (Odd amount of words)
 while x < len(l):
-    #print(l[x])
     #Insert first word into list l2 and then add 2 to x
     #and then add that word to the list
     l2.insert(x,l[x])
     x=x+2
-#print(l2)
 print(' '.join(l2))
-
-##Below is how i origionally started 
-#for x in range(len(l)):
-    #l[x] = l[x+2]
- #   print(l[x],l[x+1])

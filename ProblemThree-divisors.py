90b5a7a6ce2389a1c0f28084cc7d32ad41b8b9ce
Eoin Stankard <g00300390@gmit.ie>
2019-03-13 20:17:25 +0000

    ProblemThree Finished

--- ProblemThree-divisors.py
+++ ProblemThree-divisors.py
@@ -1,5 +1,5 @@
 #Name: Eoin Stankard
-#Date: 25/02/2019
+#Date: 13/03/2019
 #Problem Three: Write a program that prints all numbers between 1,000 and 10,000 that are divisible
 #by 6 but not 12.
 
@@ -8,3 +8,5 @@ print("Numbers divisable by 6 but not 12 between 1000-10000")
 for i in range (1000,10000,1): #Start current value of i at 1000 and count to 10000 in increments of 1
     if (i%6 == 0) and (i%12 != 0): #if the current value modulus 6 is zero and current value modulus 12 is not equal to zero
         print("\t",i) #tab in one and print the value
+        x=x+1
+print("There are",x,"Numbers divisable by 6 but not 12 between 1000-10000")#Print amount of values

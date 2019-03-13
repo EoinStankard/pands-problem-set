8f6263a05937c7484ac6601cf16d6165dc31207a
Eoin Stankard <g00300390@gmit.ie>
2019-03-13 19:56:18 +0000

    ProblemTwo Finished

--- ProblemTwo-begins-with-t.py
+++ ProblemTwo-begins-with-t.py
@@ -1,11 +1,14 @@
 #Name: Eoin Stankard
-#Date: 25/02/2019
+#Date: 13/03/2019
 #Problem Two:Write a program that outputs whether or not today is a day that begins with the
 #letter T. An example of running this program on a Thursday is as follows.
 
 import datetime
-day = datetime.datetime.today().weekday() #Gets the day of the week
-if (day == 1) or (day == 3): # 1=Tuesday and 3= Thursday
+weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]#Days of the week
+day = datetime.datetime.today().weekday() #Gets the value for day of the week
+if (day == 1) or (day==3): # 1=Tuesday and 3= Thursday
   print("Yes. Today begins with T")
+  print("Today is "+weekdays[day]) #gets the day of the week from weekdays list
 else:
-  print("No. Today does not begin with T", day)
+  print("No. Today does not begin with T")
+  print("Today is "+weekdays[day]) #gets the day of the week from weekdays list

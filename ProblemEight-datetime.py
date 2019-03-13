34b279b6ed146900e9ee4862f87f13ba5d559ce8
Eoin Stankard <g00300390@gmit.ie>
2019-03-13 19:49:39 +0000

    ProblemEight Finished

--- ProblemEight-datetime.py
+++ ProblemEight-datetime.py
@@ -1,26 +1,23 @@
 #Name: Eoin Stankard
-#Date: 11/03/2019
+#Date: 13/03/2019
 #Problem Eight: Write a program that outputs today’s date and time in the format ”Monday, January
 #10th 2019 at 1:15pm”
 import datetime
 import calendar
-dt = datetime.datetime.strftime(datetime.datetime.now(),"%Y-%m-%d-%H:%M-%S")
+dt = datetime.datetime.strftime(datetime.datetime.now(),"%Y-%m-%d-%H:%M-%S") #prints the values for the date and time
 day = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 month = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 dateAdd = ["st","nd","rd","th","th","th","th","th","th","th","th","th","th","th","th","th","th","th","th","th","st","nd","rd","th","th","th","th","th","th","th","st"]
-l = dt.split("-")
+l = dt.split("-") #Splits the date and time in line 7 into separate values
 
-dayNum = datetime.datetime.today().weekday()
-monthNum = int(l[1])
-dateNum = int(l[2])
-year = l[0]
-#hourNum = int
-hourMinuteNum =l[3]
-hourMinuteSpl = hourMinuteNum.split(":")
-#print(int(hourMinuteSpl[0]))
-if int(hourMinuteSpl[0]) >00 and int(hourMinuteSpl[0])<12:
+dayNum = datetime.datetime.today().weekday() #Gets value for day of the week
+monthNum = int(l[1]) #gets the value for month
+dateNum = int(l[2]) #gets the value for date
+year = l[0] #gets the year
+hourMinuteNum =l[3] #gets the hour of the day
+hourMinuteSpl = hourMinuteNum.split(":") #Splits the hours from the minutes of the day eg 12:30 will get the value 12
+
+if int(hourMinuteSpl[0]) >00 and int(hourMinuteSpl[0])<12: #if the hour of the day is between 01:00 and 11:00
     print(day[dayNum]+",",month[monthNum-1],str(dateNum)+dateAdd[dateNum],year,"at",hourMinuteNum+"am")#prints day
-    #print(dt)#prints day
-    #print(l)
-elif int(hourMinuteSpl[0]) >11 and int(hourMinuteSpl[0])<24:
+elif int(hourMinuteSpl[0]) >11 and int(hourMinuteSpl[0])<24:#if the hour of the day is between 00:00 and 23:00
     print(day[dayNum]+",",month[monthNum-1],str(dateNum)+dateAdd[dateNum],year,"at",hourMinuteNum+"pm")#prints day

fd547e76f269dd9d22928938841d860e1513d746
Eoin Stankard <g00300390@gmit.ie>
2019-03-18 20:33:43 +0000

    Committing ProblemSeven using Newtons Square Root Formula

--- ProblemSeven-squareroot.py
+++ ProblemSeven-squareroot.py
@@ -1,15 +1,12 @@
 #Name: Eoin Stankard
-#Date: 04/03/2019
+#Date: 18/03/2019
 #Problem Seven: Write a program that that takes a positive floating point number as input and outputs
 #an approximation of its square root.
-import math
-#Asks for an psoitive input value
-value = float(input("Please enter a positive number: "))
-dec = int(input("Number of decimal places: "))
-#Created a variable called root that would hold the value of the square root
-#Imported the math library and then used the sqrt function to calculate the square root
-root = math.sqrt(value)
-#Used the round function to round the square root to one decimal place
-ans = round(root,dec)
-print(ans)
 
+#Square root generated Newtons Square Root Formula
+rootof = float(input("Please enter Value to get square root: " ))
+estimate =6.0
+while abs((estimate*estimate)-rootof) > 0.1:
+    estimate -= ((estimate*estimate)-rootof)/(2*estimate)
+
+print(f"the square root of {rootof} is approx. {round(estimate,3)}")
